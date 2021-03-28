0646a8b374567e28d723c1ad07a3e06ea4e14368
Kyjko <Kyjko@users.noreply.github.com>
2021-03-28 22:22:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,22 +9,25 @@ import java.util.concurrent.Executors;
 public class Main extends Canvas {
     final JFrame f;
     final ArrayList<Sim> sims;
-    final float chance_of_monster = 0.1f;
+    final float chance_of_monster = 0.05f;
     final float chance_of_death_after_warning = 0.05f;
     final int N = 500;
-    final int I = 1000;
-    final int W = 1000;
+    final int I = 2560;
+    final int W = 2560;
     final int H = 500;
-    final float dist = 0.55f;
+
+    final float dist = N/3.5f;
 
     final int[] current_coward_count;
     final int[] current_altruists_count;
+    final int[] current_gbaltruists_count;
 
     boolean quit = false;
 
     enum simtype {
         Coward(),
-        Altruistic()
+        Altruistic(),
+        GBAltruistic()
     }
 
     class Sim {
@@ -102,26 +105,55 @@ public class Main extends Canvas {
                     }
 
                 }
+                case GBAltruistic: {
+                    if (this.is_alive && !this.is_done_for_one_iter) {
+                        if (r.nextFloat() <= chance_of_monster) {
+                            if (!peer.is_done_for_one_iter) {
+                                if(peer.type == this.type) {
+                                    peer.is_alive = true;
+                                } else {
+                                    peer.is_alive = false;
+                                }
+                                peer.is_done_for_one_iter = true;
+                                if (r.nextFloat() <= chance_of_death_after_warning) {
+                                    this.is_alive = false;
+
+                                } else {
+                                    sims.add(new Sim(this.id + N, this.type));
+                                }
+                            }
+
+                        } else {
+                            sims.add(new Sim(this.id + N, this.type));
+                        }
+                        this.is_done_for_one_iter = true;
+                    }
+                }
                 default: {
                 }
 
             }
 
         }
-
-
     }
 
     public void simulate() {
         Random r = new Random();
 
         for (int i = 0; i < N; i++) {
-            sims.add(new Sim(i, r.nextFloat() >= dist ? simtype.Coward : simtype.Altruistic));
+            if(i <= dist) {
+                sims.add(new Sim(i, simtype.Coward));
+            } else if(i >= dist && i <= 2*dist) {
+                sims.add(new Sim(i, simtype.Altruistic));
+            } else {
+                sims.add(new Sim(i, simtype.GBAltruistic));
+            }
         }
 
         for (int i = 0; i < I; i++) {
             int count_cowards = 0;
             int count_altruists = 0;
+            int count_gbaltruists = 0;
 
             for (int j = 0; j < N; j++) {
                 sims.get(j).doRoutine();
@@ -139,6 +171,8 @@ public class Main extends Canvas {
                         count_cowards++;
                     } else if (s.type == simtype.Altruistic) {
                         count_altruists++;
+                    } else if (s.type == simtype.GBAltruistic) {
+                        count_gbaltruists++;
                     }
                 }
             }
@@ -146,6 +180,7 @@ public class Main extends Canvas {
             //System.out.println("Cowards: " + count_cowards + " - " + "Altruists: " + count_altruists);
             current_coward_count[i] = count_cowards;
             current_altruists_count[i] = count_altruists;
+            current_gbaltruists_count[i] = count_gbaltruists;
         }
     }
 
@@ -161,6 +196,7 @@ public class Main extends Canvas {
 
         current_coward_count = new int[I];
         current_altruists_count = new int[I];
+        current_gbaltruists_count = new int[I];
 
         sims = new ArrayList<Sim>(N);
 
@@ -195,11 +231,14 @@ public class Main extends Canvas {
         for (int i = 0; i < I; i++) {
             int cc = current_coward_count[i];
             int ca = current_altruists_count[i];
-            g.setColor(Color.yellow);
-            g.fillRect((int) (offset + ((float) W / (float) I) * i), 0, W / I, cc);
-            g.setColor(Color.GREEN);
-            g.fillRect((int) (offset + ((float) W / (float) I) * i), cc, W / I, ca);
+            int cga = current_gbaltruists_count[i];
 
+            g.setColor(Color.orange);
+            g.fillRect((int) (offset + ((float) W / (float) I) * i), 0, W / I, cc);
+            g.setColor(Color.cyan);
+            g.fillRect((int) (offset + ((float) W / (float) I) * i), cc, W / I, ca);
+            g.setColor(Color.green);
+            g.fillRect((int) (offset + ((float) W / (float) I) * i), ca+cc, W / I, cga);
         }
 
         g.dispose();
